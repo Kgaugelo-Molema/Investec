9cc32a60e63cef734ef85387704c890e6a0c137b
Kgaugelo Molema <kmolema@gmail.com>
2021-03-26 20:46:00 +0200

    Add ChuckTests

---
+++ Movies/MovieTests/ChuckTests.cs
@@ -0,0 +1,19 @@
+using Movies.Controllers;
+using System;
+using System.Net.Http;
+using Xunit;
+
+namespace MovieTests
+{
+    public class ChuckTests
+    {
+        [Fact]
+        public void CanReturnCategories()
+        {
+            const string expected = "[\"animal\",\"career\",\"celebrity\",\"dev\",\"explicit\",\"fashion\",\"food\",\"history\",\"money\",\"movie\",\"music\",\"political\",\"religion\",\"science\",\"sport\",\"travel\"]";
+
+            var result = ChuckController.GetMessage();
+            Assert.Equal(expected, result.Result);
+        }
+    }
+}
--- Movies/Movies/Controllers/ChuckController.cs
+++ Movies/Movies/Controllers/ChuckController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -15,6 +16,14 @@ namespace Movies.Controllers
         public IEnumerable<string> Get()
         {
             return new[] { "Chuck" };
+        }
+
+        public static async Task<string> GetMessage()
+        {
+            var client = new HttpClient();
+            var stringTask = client.GetStringAsync("https://api.chucknorris.io/jokes/categories");
+
+            return await stringTask;
         }
     }
 }
