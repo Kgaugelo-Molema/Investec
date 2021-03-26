041fcb9822c1764e4924957785cd0ba32344e6e4
Kgaugelo Molema <kmolema@gmail.com>
2021-03-26 20:09:32 +0200

    Initial

---
+++ Movies/Movies/Controllers/ChuckController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movies.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ChuckController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "Chuck" };
+        }
+    }
+}
---
+++ Movies/Movies/Controllers/SwapiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movies.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SwapiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "Swapi" };
+        }
+    }
+}
